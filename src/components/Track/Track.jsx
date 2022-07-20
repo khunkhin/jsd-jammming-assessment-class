aaf9f15680102a4b2d3826f2085d13bed6b83ebc
Park <109514047+khunkhin@users.noreply.github.com>
2022-07-20 21:35:00 +0700

    test

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -3,12 +3,16 @@ import React, { Component } from "react";
 import "./Track.css"
 
 class Track extends Component {
+  renderAction(){
+    
+  }
+
   render() {
     return (
       <div class="Track">
         <div class="Track-information">
-          <h3><!-- track name will go here --></h3>
-          <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+          <h3>track name</h3>
+          <p>track artist | track album </p>
         </div>
         <button class="Track-action"><!-- + or - will go here --></button>
       </div>
